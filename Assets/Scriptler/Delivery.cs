9c5dcd0d2b43ce868330ed2c7176f1d1a05fb425
Kerem Gavcar <kerem1712000@hotmail.com>
2024-08-02 18:05:56 +0300

    Update

--- Assets/Scriptler/Delivery.cs
+++ Assets/Scriptler/Delivery.cs
@@ -5,27 +5,41 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    bool hasPackage;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    
+    [SerializeField] Color32 noPackageColor = new Color32(1,1,1,1);
 
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();  
+    }
+    bool hasPackage;
+    [SerializeField] float destroyDelay = 0.5f;
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision Appears!");
+
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Package")
+        // SerializeField[]
+        if (collision.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up!");
             hasPackage = true;
-            
-        }   
+            spriteRenderer.color = hasPackageColor;
+            Destroy(collision.gameObject, destroyDelay);
+
+        }
         if (collision.tag == "Customer" && hasPackage)
         {
 
             Debug.Log("Package delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
-        
+
     }
-    
+
 }
